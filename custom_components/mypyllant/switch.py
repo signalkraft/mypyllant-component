0f479d98eb5a6c348f1e6426c1d9ee1263e0dd03
Philipp <pd@signalkraft.com>
2024-02-09 19:41:29 +0100

    Fixed hot water boost switch for VRC700

--- custom_components/mypyllant/switch.py
+++ custom_components/mypyllant/switch.py
@@ -12,7 +12,7 @@ from custom_components.mypyllant.utils import (
     HolidayEntity,
     DomesticHotWaterCoordinatorEntity,
 )
-from myPyllant.enums import DHWCurrentSpecialFunction
+from myPyllant.enums import DHWCurrentSpecialFunction, DHWCurrentSpecialFunctionVRC700
 from myPyllant.utils import get_default_holiday_dates
 
 _LOGGER = logging.getLogger(__name__)
@@ -77,10 +77,10 @@ class DomesticHotWaterBoostSwitch(DomesticHotWaterCoordinatorEntity, SwitchEntit
 
     @property
     def is_on(self):
-        return (
-            self.domestic_hot_water.current_special_function
-            == DHWCurrentSpecialFunction.CYLINDER_BOOST
-        )
+        return self.domestic_hot_water.current_special_function in [
+            DHWCurrentSpecialFunction.CYLINDER_BOOST,
+            DHWCurrentSpecialFunctionVRC700.CYLINDER_BOOST,
+        ]
 
     async def async_turn_on(self, **kwargs):
         await self.coordinator.api.boost_domestic_hot_water(
